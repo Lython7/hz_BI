e1ea88cf2ea224f61991a6bca279dd54f16314f5
Lython7 <15210370876@163.com>
2018-03-12 09:14:25 +0800

    默认的

--- yoback/views.py
+++ yoback/views.py
@@ -1,5 +1,7 @@
 import os, time
 from hz_BI.settings import MEDIA_ROOT
+from datetime import datetime
+from xlrd import xldate_as_tuple
 
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
@@ -8,6 +10,15 @@ import xlrd, json
 from rest_framework import viewsets
 
 from . import serializers, models
+
+
+def handle_upload_file(file, filename):
+    path = 'media/yoback/excelData/'  # 上传文件的保存路径，可以自己指定任意的路径
+    if not os.path.exists(path):
+        os.makedirs(path)
+    with open(path + filename, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
 
 
 class ExcelToJson(object):
@@ -20,7 +31,6 @@ class ExcelToJson(object):
         # self.datalist = []
         self.filename = filename
 
-
     def readExcel(self):
         # 获取excelData文件夹中上传了的excel文件
         # try:
@@ -31,7 +41,18 @@ class ExcelToJson(object):
             for i in range(sheet.nrows):
                 dic = {}
                 for j in range(sheet.ncols):
-                    dic[sheet.cell(0, j).value] = sheet.cell(i, j).value
+                    if sheet.cell(i, j).ctype==3:
+                        # xlrd.xldate.xldate_as_datetime(table.cell(2, 2).value, 1)
+                        x = xlrd.xldate_as_datetime(sheet.cell(i, j).value, 0)
+                        if int(sheet.cell(i, j).value) > 0:
+                            # print(x)
+                            # print(type(x))
+
+                            dic[sheet.cell(0, j).value] = x.strftime('%Y-%d-%m')
+                        else:
+                            dic[sheet.cell(0, j).value] = x.strftime('%H:%M:%S')
+                    else:
+                        dic[sheet.cell(0, j).value] = sheet.cell(i, j).value
                 datalist.append(dic)
             self.datadictotal[sheet.name] = datalist
         return self.datadictotal
@@ -68,7 +89,7 @@ def upload(request):
         name = str(request.FILES['xlfile']).split('.')
         filenm = request.user.username + '-' + name[0] + str(time.strftime("%Y-%m-%d-%Hh%Mm%Ss",time.localtime())) + '.' + name[1]
         handle_upload_file(request.FILES['xlfile'], filenm)
-        # return HttpResponse('Successful')  # 此处简单返回一个成功的消息，在实际应用中可以返回到指定的页面中
+        request.session['filenm'] = filenm
 
         # 开始读excel
         data = ExcelToJson(filenm)
@@ -76,18 +97,18 @@ def upload(request):
         # datadictotal = {'data': datadic}
 
         return render(request, 'yoback/excelcheck.html', context={'datadic':datadic})
-        # return HttpResponseRedirect('yoback/excelcheck/').get_context_data({'datadic':datadic})
-        # return JsonResponse({'data':datals})
+
+    if request.method == "GET":
+        filenm = request.session.get('filenm', None)
+        if  filenm:
+            data = ExcelToJson(filenm)
+            datadic = data.readExcel()
+            print(datadic['商品分类'])
+            # 写入数据库
+            for i in datadic['商品分类'][1::]:
+                models.GoodsClassify.objects.create(i)
 
 
 def checkexcel(request):
     return render(request, 'yoback/excelcheck.html', context={})
-
-def handle_upload_file(file, filename):
-    path = 'media/yoback/excelData/'  # 上传文件的保存路径，可以自己指定任意的路径
-    if not os.path.exists(path):
-        os.makedirs(path)
-    with open(path + filename, 'wb+') as destination:
-        for chunk in file.chunks():
-            destination.write(chunk)
 
