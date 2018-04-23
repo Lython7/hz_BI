a31fa6ff714421719a865d072457b8209b53176c
Lython7 <15210370876@163.com>
2018-04-23 15:37:47 +0800

    asd

---
+++ uprofile/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.urls import path, include
+from . import views
+from dysms_python import demo_sms_send
+
+urlpatterns = [
+    url(r'^resetpwd/', views.resetit, name='resetit'),
+
+    # url(r'sendsms/(?P<cellphone>\d{11})$', demo_sms_send.send_sms, name='sendsms'),
+]
--- uprofile/views.py
+++ uprofile/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from yotools.models import SMSCode
 
 # Create your views here.
 from rest_framework.permissions import AllowAny
@@ -8,3 +9,18 @@ from rest_framework.views import APIView
 class RegistApiView(APIView):
     permission_classes = [AllowAny]
     # serializer_class = ProfileSerializer
+
+def resetit(request):
+    cellphone = request.session.get('cellphone', None)
+    code = request.POST.get('code', None)
+    passwd = request.POST.get('passwd', None)
+    sms = SMSCode.objects.get(cellphone=cellphone)
+    if code == sms.code:
+        # 修改密码
+        print(cellphone)
+        print(passwd)
+        print(code)
+        print(sms)
+    else:
+        # 验证码错误  重新输入
+        pass
