807b08f7e8da8fb7d8807e099e11a546ee1585d7
zhengwei2531 <1820468354@qq.com>
2018-06-25 13:46:53 +0800

    6.25 13:45

--- overview/views.py
+++ overview/views.py
@@ -15,7 +15,7 @@ def today_income(request):
     res = {}
 
     # 获取时间  日期
-    incometd = incomeDay()o
+    incometd = incomeDay()
     _dtnow = incometd._get_today()        # ['2018', '05', '12', '04']
 
     try: # 获取 显示哪个
@@ -30,7 +30,7 @@ def today_income(request):
     b2b_order = 0 if queryset_list[0].aggregate(Sum('amount'))['amount__sum'] == None else queryset_list[0].aggregate(Sum('amount'))['amount__sum']
     b2b_pos =  0 if queryset_list[1].aggregate(Sum('amount'))['amount__sum'] == None else queryset_list[1].aggregate(Sum('amount'))['amount__sum']
 
-    res['order_count'] = str(len(queryset_list[0])+len(queryset_list[1].values('orderN').distinct())) ###################################
+    res['order_count'] = str(len(queryset_list[0])+len(queryset_list[1].values('orderNo').distinct())) ###################################
     res['order_amount'] = str(int(b2b_order+b2b_pos)) ###################################
     res['ordered_cust_count'] = str(len(queryset_list[0].values('memberPin').distinct())) ###################################
     res['newreg_count'] = str(len(queryset_list[2])) ###################################
